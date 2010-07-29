c3de1ba5432408229ff331fe72a6f0c446e704d3
Lars George <larsgeorge@worldlingo.com>
2010-07-29 22:19:26 +0200

    Makes edit and delete work for cluster entries.

--- src/hbexplorer/views.py
+++ src/hbexplorer/views.py
@@ -30,88 +30,49 @@ from django.contrib.auth.models import User
 from desktop.lib.django_util import render, MessageException
 from django.core import urlresolvers
 from hbexplorer.models import ClusterAddress
+from hbexplorer.forms import ClusterEntryForm
 
-__users_lock = threading.Lock()
+#__users_lock = threading.Lock()
 
 def list_clusters(request):
-  return render('list_clusters.mako', request, dict(clusters=ClusterAddress.objects.all()))
-
-def delete_cluster(request, clusterid):
-  return render('list_clusters.mako', request, None)
-
-"""
-  if request.method == 'POST':
-    try:
-      user = User.objects.get(username=username)
-      user.delete()
-      # Send a flash message saying "deleted"?
-      return list_users(request)
-    except User.DoesNotExist:
-      raise MessageException("User not found.")
-  else:
-    return render("confirm.mako",
-      request,
-      dict(path=request.path, title="Delete user?"))
-"""
+    return render('list_clusters.mako', request, dict(clusters=ClusterAddress.objects.all()))
 
 def edit_cluster(request, clusterid=None):
-  return render('list_clusters.mako', request, None)
+    """
+    edit_cluster(request, clusterid = None) -> reply
 
-  """
-  edit_cluster(request, clusterid = None) -> reply
+    @type request:        HttpRequest
+    @param request:       The request object
+    @type clusterid:       string
+    @param clusterid:      Default to None, when creating a new cluster entry
+    """
 
-  @type request:        HttpRequest
-  @param request:       The request object
-  @type clusterid:       string
-  @param clusterid:      Default to None, when creating a new cluster entry
-  """
+    if clusterid is not None:
+        instance = ClusterAddress.objects.get(address=clusterid)
+    else:
+        instance = None
 
-  """
-  if username is not None:
-    instance = User.objects.get(username=username)
-  else:
-    instance = None
+    if request.method == 'POST':
+        form = ClusterEntryForm(request.POST, instance=instance)
+        if form.is_valid(): # All validation rules pass
+            form.save()
+            request.path = urlresolvers.reverse(list_users)
+            return list_clusters(request)
+    else:
+        form = ClusterEntryForm(instance=instance)
+    return render('edit_cluster.mako', request,
+        dict(form=form, action=request.path, clusterid=clusterid))
 
-  if request.method == 'POST':
-    form = UserChangeForm(request.POST, instance=instance)
-    if form.is_valid(): # All validation rules pass
-      #
-      # Check for 3 more conditions:
-      # (1) A user cannot inactivate oneself;
-      # (2) Non-superuser cannot promote himself; and
-      # (3) The last active superuser cannot demote/inactivate himself.
-      #
-      form_is_super = form.cleaned_data["is_superuser"]
-      form_is_active = form.cleaned_data["is_active"]
-      if request.user.username == username and not form_is_active:
-        raise MessageException("You cannot make yourself inactive.")
+def delete_cluster(request, clusterid):
+    if request.method == 'POST':
+        try:
+            entry = ClusterAddress.objects.get(address=clusterid)
+            entry.delete()
+            # Send a flash message saying "deleted"?
+            return list_clusters(request)
+        except ClusterAddress.DoesNotExist:
+            raise MessageException("Entry not found.")
+    else:
+        return render("confirm.mako", request,
+        dict(path=request.path, title="Delete Cluster Entry?"))
 
-      global __users_lock
-      __users_lock.acquire()
-      try:
-        if form.instance.is_superuser:
-          if not form_is_super or not form_is_active:
-            # Is there any other active superuser left?
-            all_active_su = User.objects.filter(is_superuser__exact = True,
-                                                is_active__exact = True)
-            num_active_su = all_active_su.count()
-            assert num_active_su >= 1, "No active superuser configured"
-            if num_active_su == 1:
-              raise MessageException("You cannot remove the last active "
-                                     "superuser from the configuration.")
-        else:
-          if form_is_super and not request.user.is_superuser:
-            raise MessageException("You cannot make yourself a superuser.")
-
-        # All ok
-        form.save()
-      finally:
-        __users_lock.release()
-
-      request.path = urlresolvers.reverse(list_users)
-      return list_users(request)
-  else:
-    form = UserChangeForm(instance=instance)
-  return render('edit_user.mako', request,
-    dict(form=form, action=request.path, username=username))
-  """
